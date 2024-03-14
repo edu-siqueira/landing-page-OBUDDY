be5b1aef2cc27710480ed2ed11977d76a46cacc0
Nox <eduardo.s.souza@protonmail.com>
2024-03-13 21:21:58 -0300

    corrigida as cores das imagens no tema claro

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const div = document.getElementById('webTheme')
 const root = document.querySelector(':root')
 const themeSwitch = document.getElementById('themeSwitcher')
 const themeIcon = document.getElementById('themeIcon')
-const sectionImg = document.querySelector(".section-img")
+const telaInicial = document.getElementById('telaInicial')
+const telaDeDados = document.getElementById('telaDeDados')
+const telaDeErros = document.getElementById('telaDeErros')
 const carouselIndicators = document.getElementById('carouselIndicators')
 
 themeSwitch.addEventListener('click', function(){
@@ -10,7 +12,9 @@ themeSwitch.addEventListener('click', function(){
         root.style.setProperty('--bg-color', '#F4F3F2')
         root.style.setProperty('--bg-color-2', '#e6e6e6')
         root.style.setProperty('--font-color', '#2C3E50')
-        sectionImg.src = 'assets/tela-principal-light.png'
+        telaInicial.src = 'assets/tela-principal-light.png'
+        telaDeDados.src = 'assets/tela-de-dados-light.png'
+        telaDeErros.src = 'assets/tela-de-erros-light.png'
         carouselIndicators.dataset.bsTheme = 'dark'
         div.dataset.theme = 'light'
         themeIcon.className = 'fa-solid fa-moon'
@@ -18,7 +22,9 @@ themeSwitch.addEventListener('click', function(){
         root.style.setProperty('--bg-color', '#272725')
         root.style.setProperty('--bg-color-2', '#1F1E1c')
         root.style.setProperty('--font-color', '#b8a554')
-        sectionImg.src = 'assets/tela-inicial.png'
+        telaInicial.src = 'assets/tela-inicial.png'
+        telaDeDados.src = 'assets/tela-de-dados.png'
+        telaDeErros.src = 'assets/tela-de-erros.png'
         carouselIndicators.dataset.bsTheme = 'light'
         div.dataset.theme = 'dark'
         themeIcon.className = 'fa-solid fa-sun'
